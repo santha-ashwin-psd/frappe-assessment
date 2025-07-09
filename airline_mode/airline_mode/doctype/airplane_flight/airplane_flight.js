a246be39e84aa318f00678cad0b7180e5a5d43ff
santha-ashwin-psd <santhaashwin@psdigitise.com>
2025-07-09 12:58:29 +0530

    day3 commit

--- airline_mode/airline_mode/doctype/airplane_flight/airplane_flight.js
+++ airline_mode/airline_mode/doctype/airplane_flight/airplane_flight.js
@@ -6,3 +6,16 @@
 
 // 	},
 // });
+frappe.ui.form.on('Flight Crew Member', {
+    form_render: function(frm, cdt, cdn) {
+        // Set query when the row is rendered
+        frappe.meta.get_docfield("Flight Crew Member", "crew_member", frm.doc.name).get_query = function() {
+            return {
+                query: "frappe.core.doctype.user.user.user_query",
+                filters: {
+                    'role': 'Flight Crew Member'
+                }
+            };
+        };
+    }
+});
